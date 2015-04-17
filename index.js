384d8b655c0e7219bebcb3011edb05bb6f8f09e4
Emil Abraham <emilabraham1414@gmail.com>
2015-04-17 15:25:33 -0400

    Added bluebird promise library and able to make a request to get data

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var request = require('request');
-var options = require('options');
+var options = require('./options');
+var p = require('bluebird');
+var request = p.promisify(require('request'));
+
+request(options.stats(25523944, 'na'))
+.spread(function (response, body) {
+  var parsed;
+  try {
+    parsed = JSON.parse(body);
+  }
+  catch (error){
+    return console.error('Error parsing JSON: ', error);
+  }
+  console.log(parsed);
+},
+function (error) {
+  return console.error('Request threw an error: ', error);
+});
