3f99bb779023cfafd55a5d7a146abc47fc4999c2
Emil Abraham <emilabraham1414@gmail.com>
2015-04-17 23:08:32 -0400

    Refactored a bit

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ var options = require('./options');
 var p = require('bluebird');
 var request = p.promisify(require('request'));
 
-request(options.stats(25523944, 'na'))
-.spread(function (response, body) {
-  var parsed;
-  try {
-    parsed = JSON.parse(body);
+// Generic function that returns an error when a promise is rejected
+function reject (message, error) {
+  return console.error(message, error);
+}
+
+// Get the stats from given summonerId and region
+function getStats (summonerId, region) {
+  function fulfill (response, body) {
+    var parsed;
+    try {
+      parsed = JSON.parse(body);
+    }
+    catch (error){
+      return console.error('Error parsing JSON: ', error);
+    }
+    console.log(parsed);
   }
-  catch (error){
-    return console.error('Error parsing JSON: ', error);
-  }
-  console.log(parsed);
-},
-function (error) {
-  return console.error('Request threw an error: ', error);
-});
+
+  request(options.getStats(summonerId, region))
+  .spread(fulfill, reject.bind(null, 'Error retrieving stats'));
+}
+
+// main
+getStats(25523944, 'na');
--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 var configs = require('./utils/config-utils');
 var Options = {
-  stats: function stats (summonerId, region) {
+  getStats: function getStats (summonerId, region) {
     return {
-      url: 'htt[s://na.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerId + '/ranked',
+      url: 'https://na.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerId + '/ranked',
       method: 'GET',
       qs: {
         api_key: configs.apikey
