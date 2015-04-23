2f502056a620314bc618b55d03433b57f48fbfe0
Emil Abraham <emilabraham1414@gmail.com>
2015-04-23 11:41:47 -0400

    Able to post data and display it

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
+//Routes
+var bodyParser = require('body-parser');
+
 module.exports = function(app) {
   app.get('/', function(request, response) {
     response.render('index', { title: 'Hey', message: 'Hello there!' });
   });
   app.get('/about', function (request, response) {
     response.render('about.html');
+  });
+  app.post('/', bodyParser.urlencoded({extended: true}), function(request, response) {
+    console.log(request.body.summonerName);
+    response.render('list', { summonerName: request.body.summonerName });
   });
 };
