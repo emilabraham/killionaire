58421f73bcd6c58635a4489a429ace5eba6b71a5
Emil Abraham <emilabraham1414@gmail.com>
2015-04-24 10:02:22 -0400

    Passing in JSON blurb and fixed binding error

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 //Routes
 var bodyParser = require('body-parser');
-var eatShit = require('../index');
+var apicalls = require('../index');
 
 module.exports = function (app) {
   app.get('/', function(request, response) {
@@ -14,10 +14,11 @@ module.exports = function (app) {
   app.post('/', bodyParser.urlencoded({extended: true}), function(request, response) {
     var summonerName = request.body.summonerName;
     console.log(summonerName);
-    eatShit.getSummonerId(summonerName, 'na')
-    .then(eatShit.getStats.bind(null, 'na'), console.log)
-    .then(eatShit.constructJSON, console.log)
-    .then(response.render.bind(null, 'list'), console.log);
-    //response.render('list', { summonerName: request.body.summonerName });
+    apicalls.getSummonerId(summonerName, 'na')//Returns the summonerId
+    .then(apicalls.getStats.bind(null, 'na'), console.log)//Returns the stats
+    .then(apicalls.constructJSON, console.log)//Returns a JSON blob
+    .then(function (json) {
+      response.render('list', json);
+    }, console.log);
   });
 };
