b15bd8cc126b67f859c1cbd3afff165c459b4a62
Emil Abraham <emilabraham1414@gmail.com>
2015-05-04 12:56:13 -0400

    Made routes deal with promises using express-promise-router

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-require('./routes/index.js')(app);
+//require('./routes/index.js');
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.use('/', require('./routes/index'));
 app.use(express.static('public'));
 var server = app.listen(3000, function() {
   console.log('Started server on port 3000');
