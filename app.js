491725f8840a070391999f842e1dee3f87c3411b
uhkis <janne@jannehakala.com>
2015-05-07 20:21:06 +0300

    Use readable html.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 require('./routes/index.js')(app);
 app.set('views', './views');
 app.set('view engine', 'jade');
+app.set('view options', { pretty: true });
+app.locals.pretty = true;
 app.use(express.static('public'));
 var server = app.listen(3000, function() {
   console.log('Started server on port 3000');
