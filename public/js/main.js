a73e5c63e5b4db2cfbd031134111391619870515
Uhkis <janne@jannehakala.com>
2015-05-08 09:32:39 +0300

    Show spinner in input rather than after it.

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
   $('#summonerForm').on('submit', function(e) {
     e.preventDefault();
-    $('#result').html('<img id="spinner" src="img/spinner.gif">');
-    var summoner = $('input[name="summonerName"]').val();
+    var $input = $('input[name="summonerName"]');
+    var spincss = {
+      'background-image': 'url(img/spinner.gif)',
+      'background-position': '250px center',
+      'background-repeat': 'no-repeat'
+    }
+    $input.css(spincss);
+    var summoner = $input.val();
     $.post('search', $(this).serialize())
     .done(function(data) {
       $('#result').html(data);
     }).fail(function(xhr, status, error) {
       $('#result').html(xhr.responseText);
     }).always(function() {
-      $('#spinner').remove();
+      $input.css('background-image', '');
     });
   });
 });
