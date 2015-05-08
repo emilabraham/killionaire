05afba41f497e5e1b92fbf10444b8f9b4a9b878a
uhkis <janne@jannehakala.com>
2015-05-08 06:43:56 +0300

    Show error sent by backend if ajax fails.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $('#summonerForm').on('submit', function(e) {
     e.preventDefault();
-    $('#result').html('<img src="img/spinner.gif">');
+    $('#result').html('<img id="spinner" src="img/spinner.gif">');
     var summoner = $('input[name="summonerName"]').val();
     $.post('search', $(this).serialize())
     .done(function(data) {
       $('#result').html(data);
+    }).fail(function(xhr, status, error) {
+      $('#result').html(xhr.responseText);
+    }).always(function() {
+      $('#spinner').remove();
     });
   });
 });
