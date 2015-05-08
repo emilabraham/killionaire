e5bff97fc1a8b900db21ff58f0bd68ae9731c21a
Uhkis <janne@jannehakala.com>
2015-05-08 09:46:38 +0300

    Add animation to results.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#summonerForm').on('submit', function(e) {
     e.preventDefault();
+    $('#result').fadeOut(400);
     var $input = $('input[name="summonerName"]');
     var spincss = {
       'background-image': 'url(img/spinner.gif)',
@@ -11,7 +12,7 @@ $(document).ready(function() {
     var summoner = $input.val();
     $.post('search', $(this).serialize())
     .done(function(data) {
-      $('#result').html(data);
+      $('#result').html(data).fadeIn(400);
     }).fail(function(xhr, status, error) {
       $('#result').html(xhr.responseText);
     }).always(function() {
