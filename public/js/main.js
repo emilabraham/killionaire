d691a1bc87fdd9dd6fc8d6b0ad5354ee8c63d01e
Uhkis <janne@jannehakala.com>
2015-05-12 12:10:42 +0300

    Add permalink functionality.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
+  if (location.hash) {
+    hashSearch();
+  }
+
+  $(window).on('hashchange', function() {
+    hashSearch();
+  });
+
   $('#region').on('click', function(e) {
     $('#options').toggle();
   });
+
   $('.option').on('click', function() {
     $('#region').text($(this).text());
     $('#options').hide();
   });
+
   $('#summonerForm').on('submit', function(e) {
     e.preventDefault();
     $('#result').fadeOut(400);
+    var summoner = $('input[name="summonerName"]').val();
+    var region = $('#region').text().toLowerCase();
+    getData(region, summoner);
+  });
+
+  function hashSearch() {
+    var params = location.hash.replace('#', '').split(':');
+    $('#region').text(params[0].toUpperCase());
+    $('input[name="summonerName"]').val(params[1]);
+    getData(params[0].toLowerCase(), params[1]);
+  }
+
+  function getData(region, summoner) {
     var $input = $('input[name="summonerName"]');
     var spincss = {
       'background-image': 'url(img/spinner.gif)',
@@ -16,8 +39,8 @@ $(document).ready(function() {
       'background-repeat': 'no-repeat'
     }
     $input.css(spincss);
-    var summoner = $input.val();
-    var post = $(this).serialize() + '&region=' + $('#region').text().toLowerCase();
+
+    post = 'region=' + region + '&summonerName=' + summoner;
     $.post('search', post)
     .done(function(data) {
       $('#result').html(data);
@@ -28,10 +51,14 @@ $(document).ready(function() {
           $('#result table').fadeIn(400);
         });
       });
+      hash = '#' + region + ':' + summoner;
+      $link = $('<a>').attr('href', hash).text('Permalink');
+      $('#summoner h1').after($link);
     }).fail(function(xhr, status, error) {
       $('#result').html(xhr.responseText).show();
     }).always(function() {
-      $input.css('background-image', '');
+      $('input[name="summonerName"]').css('background-image', '');
     });
-  });
+  }
+
 });
